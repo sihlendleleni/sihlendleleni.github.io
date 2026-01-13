6b4677867f54dd74bafbd68b5e899fd9dffb8204
sihlendleleni <sihle.ndleleni@icloud.com>
2026-01-13 05:39:31 +0000

    Deploying to gh-pages from @ sihlendleleni/sihlendleleni.github.io@fdc3afaae81b8fc552deeb3f81a8358812637a8d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -387,9 +387,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-",
-          title: '',
-          description: "",
+          section: "News",},{id: "projects-the-economics-of-taste-an-econometric-analysis-of-personal-music-consumption",
+          title: 'The Economics of Taste: An Econometric Analysis of Personal Music Consumption',
+          description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-a-map-of-taste-four-years-90-000-choices",
